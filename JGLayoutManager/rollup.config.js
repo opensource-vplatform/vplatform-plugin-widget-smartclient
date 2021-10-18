87aae1cc33151a11a3acf1d3713a4d7cc3c1a695
onlylife59 <octonber@qq.com>
2021-10-18 14:33:27 +0800

    Task20211018012
    bug处理

--- JGLayoutManager/rollup.config.js
+++ JGLayoutManager/rollup.config.js
@@ -11,6 +11,9 @@ export default {
         babel({
             runtimeHelpers: true
         }),
+        /*terser({
+            compress:{defaults:false,join_vars:false,drop_debugger:false}
+        })*/
         terser()
     ]
 };
