9a46ffa337cfdfd99781314003ba8abf359a8516
onlylife59 <octonber@qq.com>
2022-01-27 17:29:30 +0800

    Task20220117090

--- JGFormLayout/src/items/JGRichTextEditorFormItem.js
+++ JGFormLayout/src/items/JGRichTextEditorFormItem.js
@@ -1,18 +1,18 @@
-
 isc.ClassFactory.defineClass("JGRichTextEditorFormItem", "V3RichTextEditorItem");
-isc.JGRichTextEditorFormItem.addProperties({
-});
+isc.JGRichTextEditorFormItem.addProperties({});
 
 isc.JGRichTextEditorFormItem.addMethods({
-    init : function(){
+    init: function () {
         var _this = this;
-        this.blurDataSync = (function(){
-            return function(){
+        this.blurDataSync = (function () {
+            return function () {
                 var newValue = _this.getContent();
-                widgetDatasource.setSingleValue(_this.Code, newValue);
+                _this.form.setValue(_this.ColumnName, newValue);
+                _this.form._dataSyn(_this.widgetId);
+                //widgetDatasource.setSingleValue(_this.Code, newValue);
             }
         })();
-        this.Super("init",arguments);
+        this.Super("init", arguments);
         if (this.form && this.form._putWidgetContextProperty) {
             this.form._putWidgetContextProperty(this.Code, 'widgetObj', this);
         }
